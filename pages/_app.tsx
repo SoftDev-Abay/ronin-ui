ca0eaf69009ea300e1e7a716c7f5d4fc3ff6cc11
SoftDev-Abay <abay.aliev.03@gmail.com>
2024-07-23 18:32:04 +0500

    deleted leftover code/deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,6 @@ import "../styles/global.scss";
 import { AppCacheProvider } from "@mui/material-nextjs/v13-pagesRouter";
 import { NextIntlClientProvider } from "next-intl";
 import { useRouter } from "next/router";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { useEffect, useState } from "react";
 import Preloader from "@/app/pages/Preloader/Preloader";
 export default function App({ Component, pageProps }: AppProps) {
@@ -27,7 +25,6 @@ export default function App({ Component, pageProps }: AppProps) {
             timeZone="Europe/Vienna"
             messages={pageProps.messages}
           >
-            <ToastContainer />
             <Component {...pageProps} />
           </NextIntlClientProvider>
         </AppCacheProvider>
