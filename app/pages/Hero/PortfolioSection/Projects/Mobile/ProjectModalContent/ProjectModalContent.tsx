6d8c48a520017488cbd535eb06ca22bd51e03629
SoftDev-Abay <abay.aliev.03@gmail.com>
2024-07-26 13:40:24 +0500

    fixed the modal slider bug, added button disabled

--- app/pages/Hero/PortfolioSection/Projects/Mobile/ProjectModalContent/ProjectModalContent.tsx
+++ app/pages/Hero/PortfolioSection/Projects/Mobile/ProjectModalContent/ProjectModalContent.tsx
@@ -1,16 +1,12 @@
-import React, { useEffect, useRef } from "react";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Swiper as SwiperClass } from "swiper";
-
-import "swiper/css";
-import "swiper/css/navigation";
-
+import React, { useState, useEffect, useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
 import "./style.scss";
-
-import { Navigation } from "swiper/modules";
 import ChevroletRightIcon from "@/app/icons/ChevroletRightIcon";
 import ChevroletLeftIcon from "@/app/icons/ChevroletLeftIcon";
+import { Navigation } from 'swiper/modules';
 
 interface Project {
   title: string;
@@ -23,73 +19,75 @@ interface ProjectModalContentProps {
   project: Project;
 }
 
-const ProjectModalContent = ({
-  project: { title, description, imgsURL, categories },
-}: ProjectModalContentProps) => {
-  const swiperRef = useRef<SwiperClass | null>(null);
+const ProjectModalContent: React.FC<ProjectModalContentProps> = ({ project }) => {
+  const [isBeginning, setIsBeginning] = useState(true);
+  const [isEnd, setIsEnd] = useState(false);
+  const swiperRef = useRef<SwiperCore | null>(null);
 
   useEffect(() => {
-    if (swiperRef.current) {
-      swiperRef.current.navigation.update();
+    const swiperInstance = swiperRef.current;
+
+    if (swiperInstance) {
+      swiperInstance.on('reachBeginning', () => {
+        setIsBeginning(true);
+        setIsEnd(false);
+      });
+
+      swiperInstance.on('reachEnd', () => {
+        setIsEnd(true);
+        setIsBeginning(false);
+      });
+
+      swiperInstance.on('fromEdge', () => {
+        setIsBeginning(swiperInstance.isBeginning);
+        setIsEnd(swiperInstance.isEnd);
+      });
     }
+
+    return () => {
+      if (swiperInstance) {
+        swiperInstance.off('reachBeginning');
+        swiperInstance.off('reachEnd');
+        swiperInstance.off('fromEdge');
+      }
+    };
   }, []);
 
-  const handlePrev = () => {
-    swiperRef.current?.slidePrev();
-  };
-
-  const handleNext = () => {
-    swiperRef.current?.slideNext();
-  };
   return (
     <div className="project-modal-content">
       <div className="swiper-content-wrapper">
         <Swiper
+          onSwiper={(swiper) => swiperRef.current = swiper}
           spaceBetween={20}
           modules={[Navigation]}
-          className="mySwiper"
           navigation={{
             nextEl: ".custom-swiper-button-next",
             prevEl: ".custom-swiper-button-prev",
           }}
-          onSwiper={(swiper) => (swiperRef.current = swiper)}
         >
-          {imgsURL.map((imgURL) => (
+          {project.imgsURL.map((imgURL) => (
             <SwiperSlide key={imgURL}>
-              <img src={imgURL} alt={title} />
+              <img src={imgURL} alt={project.title} />
             </SwiperSlide>
           ))}
         </Swiper>
-        <div
-          className="custom-swiper-button custom-swiper-button-prev"
-          onClick={handlePrev}
-        >
+        <div className={`custom-swiper-button custom-swiper-button-prev ${isBeginning ? 'disabled' : ''}`}>
           <ChevroletLeftIcon width={44} height={44} />
         </div>
-        <div
-          className="custom-swiper-button custom-swiper-button-next"
-          onClick={handleNext}
-        >
+        <div className={`custom-swiper-button custom-swiper-button-next ${isEnd ? 'disabled' : ''}`}>
           <ChevroletRightIcon width={44} height={44} />
         </div>
       </div>
 
       <div className="project-card-info">
-        <h3 className="project-card-title">{title}</h3>
-        <p className="project-card-description">{description}</p>
+        <h3 className="project-card-title">{project.title}</h3>
+        <p className="project-card-description">{project.description}</p>
         <div className="project-card-categories">
-          {categories.map((category, index) => {
-            return (
-              <>
-                <span key={category} className="project-card-category">
-                  {category}
-                </span>
-                {index !== categories.length - 1 && (
-                  <span className="project-card-category">•</span>
-                )}
-              </>
-            );
-          })}
+          {project.categories.map((category, index) => (
+            <span key={category} className="project-card-category">
+              {category}{index !== project.categories.length - 1 && ' • '}
+            </span>
+          ))}
         </div>
       </div>
     </div>
