336e466c321b96718826ceb5f0887ebac287b441
SoftDev-Abay <abay.aliev.03@gmail.com>
2024-07-31 11:18:00 +0500

    changed footer accordion to design

--- app/pages/Wrapper/Footer.tsx
+++ app/pages/Wrapper/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="padding-wrapper footer-container">
-        <div className="main">
+        <div className="left">
           <div className="info">
             <div className="navigation">
               <a href="#services">Services</a>
@@ -24,14 +24,18 @@ const Footer = () => {
             </div>
             <p className="email">hi@ronindsgn.com</p>
           </div>
+          <p className="company">
+            <span className="long-text">
+              (с) 2024 Individual Entrepreneur "Amangali"
+            </span>
+            <span className="short-text">(с) 2024 Ronin, LLC</span>
+          </p>
+        </div>
+
+        <div className="right">
           <div className="logo-glass-3d">
             <img src="/imgs/logo-3d-glass-cropped.png" alt="" />
           </div>
-        </div>
-        <div className="copyright">
-          <p className="company">
-            {"(с)"} 2024 Individual Entrepreneur "Amangali"
-          </p>
           <p className="rights">All rights reserved</p>
         </div>
       </div>
